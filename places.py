7b4c3bec6a17de7882313c68b83cce3cd240bebe
Cameron White <cawhite@pdx.edu>
2013-09-08 07:34:03 -0700

    Reimplemented using SQLAlchemy

--- places.py
+++ places.py
@@ -3,55 +3,66 @@
 import sqlite3
 import IPython
 import os
+from sqlalchemy import Column, Integer, String, PickleType
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref, scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+Session = scoped_session(sessionmaker())
+engine = None
+
+class Place(Base):
+    __tablename__ = 'Places'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    user = Column(String)
+    password = Column(String)
+    host = Column(String) 
+    port = Column(Integer)
+
+    def __init__(self, name, host, port, user, password):
+        self.name = name
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+    def __repr__(self, name):
+        return '<Place({})>'.format(name)
 
 class Places(object):
-    def __init__(self, path):
-        """ """
+    def __init__(self, database_filename):
+        global engine
+        engine = create_engine(
+            'sqlite:///{}'.format(database_filename))
+        Session.configure(
+            bind=engine, autoflush=False, 
+            expire_on_commit=False)
         path = IPython.utils.path.expand_path(path)
-        self.connection = sqlite3.connect(path)  
-        self.cursor = self.connection.cursor()
-        self._createTables()
-    def close(self):
-        """ """
-        self.connection.close()
-    def add(self, place_name, user_name, host, ssh_port):
-        """ """
-        self.cursor.execute('''
-            INSERT INTO places (
-                    place_name,
-                    user_name, 
-                    host, 
-                    ssh_port 
-                ) VALUES (?,?,?,?)
-        ''', (place_name, user_name, host, ssh_port))
-        self.connection.commit()
-    def list(self):
-        """ """
-        self.cursor.execute('''
-            SELECT 
-                place_name, user_name, host, ssh_port 
-            FROM places
-        ''')
-        return self.cursor.fetchall()
-    def connect(self, place_name):
-        self.cursor.execute('''
-            SELECT user_name, host, ssh_port 
-            FROM places
-            WHERE place_name=?
-        ''', (place_name,))
-        place = self.cursor.fetchone()
-        command = 'ssh {}@{} -p {}'.format(place[0],place[1],place[2])
+        Base.metadata.create_all(engine)
+        Session.commit()
+
+    def add(self, name, host, port, user, password):
+        """ Add a place """
+        Session.add(Place(name, host, port, user, password))
+        Session.commit()
+
+    def get(self, name):
+        """ Get the place by its name """
+        return Session.query(Place).\
+            filter(Place.name == name).\
+            one()
+
+    def getAll(self):
+        """ Return a list of all the places """
+        return Session.query(Place).all()
+
+    def connect(self, name):
+        """ Connect to the place by its name """
+        place = self.get(name)
+        command = 'ssh {}@{} -p {}'.format(
+            place.user, place.host ,place.port)
         os.system(command)
-    def _createTables(self):
-        """ """
-        try:
-            self.cursor.execute('''
-                CREATE TABLE places (
-                    place_name TEXT PRIMARY KEY,
-                    user_name TEXT,
-                    host TEXT,
-                    ssh_port INTEGER
-                )
-            ''')
-        except sqlite3.OperationalError as exception:
-            print('ERROR')
